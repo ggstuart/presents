919045720c0fb28adf809b42f145a875b6402b3f
Fania Ra <fania@web.de>
2019-10-07 19:44:45 +0100

    change some colours and pointer events

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
 const queryString = location.search;
-console.log(queryString);
+// console.log(queryString);
 
 const regex = /(?<user>[\w]+)_(?<repo>[\S]+)_(?<file>[\S]+)/g;
 const match = regex.exec(queryString);
-console.log(match.groups);
+// console.log(match.groups);
 
 const url = `https://raw.githubusercontent.com/${match.groups.user}/${match.groups.repo}/master/${match.groups.file}.md`;
-console.log(url);
+// console.log(url);
 
 const slides = document.querySelector(".slides");
 slides.innerHTML = `
