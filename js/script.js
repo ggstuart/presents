6e0dd68d10fb965c75f4499857724ea9e819a8a7
Fania Ra <fania@web.de>
2019-10-07 19:04:06 +0100

    change url query string

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 
 const queryString = location.search;
-// console.log(queryString);
+console.log(queryString);
 
-const regex = /(?<user>[\w]+)\/(?<repo>[\S]+)\/(?<file>[\S]+)/g;
+const regex = /(?<user>[\w]+)-(?<repo>[\S]+)-(?<file>[\S]+)/g;
 const match = regex.exec(queryString);
+console.log(match.groups);
 
 const url = `https://raw.githubusercontent.com/${match.groups.user}/${match.groups.repo}/master/${match.groups.file}.md`;
-// console.log(url);
+console.log(url);
 
 const slides = document.querySelector(".slides");
 slides.innerHTML = `
 <section data-markdown="${url}"
          data-separator-vertical="^\n\n"
-         data-separator-notes="^Note:">
+         data-separator-notes="^Note:"
+         data-charset="iso-8859-15">
 </section>
 `;
+
+const title = document.querySelector("title");
+title.innerHTML = `${match.groups.repo} ${match.groups.file}`;
