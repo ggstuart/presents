a7954f53ed2571ba98dadbcf051347821f8eb9d2
Fania Ra <fania@web.de>
2019-10-13 17:19:42 +0100

    add more detail to default presentation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 
-const queryString = location.search;
+let queryString = location.search;
 // console.log(queryString);
 // console.log(location);
+
+if (location.search.includes("%2F")) {
+  queryString = queryString.replace("%2F", "/");
+  // console.log(queryString);
+  history.pushState('', '', queryString);
+}
 
 const rgx = /([a-zA-Z\-0-9\/]+)/g;
 // const regex = /(?<user>[\w]+)_(?<repo>[\S]+)_(?<file>[\S]+)/g;
@@ -11,11 +17,6 @@ const match = queryString.match(rgx);
 
 if(match) {
 
-  // let fullname = match.groups.file;
-  // console.log(fullname);
-  // fullname.contains("print-pdf") ? fullname.slice()
-
-  // const url = `https://raw.githubusercontent.com/${match.groups.user}/${match.groups.repo}/master/${match.groups.file}.md`;
   const url = `https://raw.githubusercontent.com/${match[0]}/${match[1]}/master/${match[2]}.md`;
   // console.log(url);
 
@@ -31,46 +32,3 @@ if(match) {
   const title = document.querySelector("title");
   title.innerHTML = `${match[1]} ${match[2]}`;
 }
-
-
-
-
-// Printing and PDF exports
-
-
-// let pr = document.getElementById("print");
-// pr.addEventListener("click", () => {
-
-//   console.log("print rdy");
-
-//   let link = document.createElement('link');
-//   link.rel = 'stylesheet';
-//   link.href = 'css/print/pdf.css';  // 'css/print/paper.css'
-//   document.getElementsByTagName('head')[0].appendChild(link);
-
-// });
-
-
-
-
-// // remove pdf if present
-// if(match.groups.file.includes("_pdf")) {
-
-//   console.log("yes print");
-
-
-//   let link = document.createElement('link');
-//   link.rel = 'stylesheet';
-//   link.href = location.search.match(/pdf/gi) ? 
-//               'css/print/pdf.css' : 
-//               'css/print/paper.css';
-//   document.getElementsByTagName('head')[0].appendChild(link);
-
-
-  
-// }
-// else {
-//   console.log("no print");
-// }
-
-
