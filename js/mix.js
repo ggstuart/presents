bf9d8fac3d7a303b97471d2b4960ad262494a7ea
Fania Ra <fania@web.de>
2019-11-25 14:33:46 +0000

    make mix work automatically without _mix parameter

--- js/mix.js
+++ js/mix.js
@@ -8,59 +8,55 @@ if (location.search.includes("%2F")) {
 
 const rgx = /([a-zA-Z\-0-9\/]+)/g;
 const match = queryString.match(rgx);
+// console.log(match);
 
-if(match === null) {
-  delayed_init();
+if (match === null) {
+  delayed_init();  // run default presentation from index.html
 } else {
+  const url = `https://raw.githubusercontent.com/${match[0]}/${match[1]}/master/${match[2]}.md`;
 
-  if(match.length === 3 || match.length === 4 && match[3] === "print-pdf") {
-    const url = `https://raw.githubusercontent.com/${match[0]}/${match[1]}/master/${match[2]}.md`;
-
-    const slides = document.querySelector(".slides");
-    slides.innerHTML = `
-    <section data-markdown="${url}"
-            data-separator-vertical="^\n\n"
-            data-separator-notes="^Note:"
-            data-charset="utf-8">
-    </section>
-    `;
-    const title = document.querySelector("title");
-    title.innerHTML = `${match[1]} ${match[2]}`;
-    delayed_init();
-  } 
-
-  if(match.length > 3 && match[3] === "mix") {
-    const url = `https://raw.githubusercontent.com/${match[0]}/${match[1]}/master/${match[2]}.md`;
-
-    fetch(url)
-      .then(function(response) {
-        return response.text();
-      })
-      .then(function(text) {
-        getURLs(text);
-      })
-      .catch(function(err) {
-        console.log('Fetch Error: ', err);
-      });
-  }
+  fetch(url)
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(text) {
+      text.startsWith("# Contents") ? loadMultiple(text) : loadSingle();
+      delayed_init();
+    })
+    .catch(function(err) {
+      console.log("Fetch Error: ", err);
+    });
 }
 
-function getURLs(text) {
-  let fileNames = text.split("\n"); 
+function loadSingle() {
+  const url = `https://raw.githubusercontent.com/${match[0]}/${match[1]}/master/${match[2]}.md`;
   const slides = document.querySelector(".slides");
-  slides.innerHTML = "";
-
-  fileNames.forEach(f => {
-    let all = f.trim();
-    let details = all.split(" ");
-    const url = `https://raw.githubusercontent.com/${details[0]}/${details[1]}/master/${details[2]}.md`;
-    slides.innerHTML += `
+  slides.innerHTML = `
     <section data-markdown="${url}"
             data-separator-vertical="^\n\n"
             data-separator-notes="^Note:"
             data-charset="utf-8">
     </section>
+  `;
+  const title = document.querySelector("title");
+  title.innerHTML = `${match[1]} ${match[2]}`;
+}
+
+
+function loadMultiple(text) {
+  let lines = text.trim().split("\n").splice(2); 
+  const slides = document.querySelector(".slides");
+  slides.innerHTML = "";  // remove default presentation
+  lines.forEach(line => {
+    let cleanedLine = line.trim();
+    let details = cleanedLine.split(" ");
+    const url = `https://raw.githubusercontent.com/${details[0]}/${details[1]}/master/${details[2]}.md`;
+    slides.innerHTML += `
+      <section data-markdown="${url}"
+              data-separator-vertical="^\n\n"
+              data-separator-notes="^Note:"
+              data-charset="utf-8">
+      </section>
     `;
   });
-  delayed_init();
 }
